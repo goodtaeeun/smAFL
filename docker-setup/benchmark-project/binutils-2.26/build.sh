73e84f8494e0a87a509987bb169c97354ec16fe2
goodtaeeun <goodtaeeun@kaist.ac.kr>
2023-06-01 14:58:19 +0900

    git add infra to build target benchmark

---
+++ docker-setup/benchmark-project/binutils-2.26/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+URL="http://ftp.gnu.org/gnu/binutils/binutils-2.26.tar.gz"
+DIRNAME="binutils-2.26"
+ARCHIVE=$DIRNAME".tar.gz"
+CONFIG_OPTIONS="--disable-shared --disable-gdb \
+                 --disable-libdecnumber --disable-readline \
+                 --disable-sim --disable-ld"
+
+wget $URL -O $ARCHIVE
+rm -rf $DIRNAME
+tar -xzf $ARCHIVE || exit 1
+cd $DIRNAME
+
+./configure $CONFIG_OPTIONS || exit 1
+
+
+## sed with $BUG_NAME and $STR_TYPE
+
+patch -p0 < $(dirname $0)/patches/${BUG_NAME}_${STR_TYPE}.patch
+
+## Parallel building according to https://github.com/aflgo/aflgo/issues/59
+## Although an issue with parallel building is observed in libxml (https://github.com/aflgo/aflgo/issues/41), 
+## We have not yet encountered a problem with binutils.
+make -j || exit 1
+cd ../
+cp $DIRNAME/binutils/cxxfilt ./cxxfilt || exit 1
