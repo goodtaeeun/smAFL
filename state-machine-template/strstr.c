47440b345dc3ff3a6f27ef8c2380f61501215fa1
kaist-kihwan <62145252+kaist-kihwan@users.noreply.github.com>
2023-06-15 23:17:41 +0900

    implement strstr

--- state-machine-template/strstr.c
+++ state-machine-template/strstr.c
@@ -1,9 +1,37 @@
+#include <stdio.h>
+
 int main(){
 
 char num_str[500];
 scanf("%s", num_str);
 
 // implement state machine
+int current_state = 0;
+int i;
+for(i=0;;i++) {
+  switch (current_state) {
+  case 0:
+    if (num_str[i] == '1') current_state = 1;
+    else current_state = 0;
+    break;
+  case 1:
+    if (num_str[i] == '2') current_state = 2;
+    else current_state = 0;
+    break;
+  case 2:
+    if (num_str[i] == '3') current_state = 3;
+    else current_state = 0;
+    break;
+  case 3:
+    if (num_str[i] == '4') current_state = 4;
+    else current_state = 0;
+    break;
+  case 4:
+    current_state = 4;
+    break;
+  }
+  if (num_str[i] == '\0') break;
+}
 
 if (strstr(num_str, "1234"))
   printf("Crash!!");
