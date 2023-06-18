960236d9a82286ad1803a2085bd34bf1755f0095
oojahooo <oojahooo@gmail.com>
2023-06-18 20:04:27 +0900

    give priviege to container

--- launch.sh
+++ launch.sh
@@ -3,4 +3,4 @@
 PROJECT_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 yMAKE_HOME="$PROJECT_HOME/smake"
 
-docker run --rm -it --memory=4g -v$SMAKE_HOME:/smake smafl
+docker run --rm -it --privileged --memory=4g -v$SMAKE_HOME:/smake smafl
